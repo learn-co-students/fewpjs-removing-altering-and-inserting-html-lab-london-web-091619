5f6a681418ca65419e1493e3df600ac9531ec8da
idmyn <hello@davidmyno.rs>
2019-10-28 14:16:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.querySelector("main").remove()
+const newHeader = document.createElement("h1")
+newHeader.setAttribute("id", "victory")
+newHeader.innerHTML = "David is the champion"
+document.body.appendChild(newHeader)
